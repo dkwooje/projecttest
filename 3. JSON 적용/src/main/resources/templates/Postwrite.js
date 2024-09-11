ff9aeaefc9c0b74fa260fcc006556fabeff88c27
wooje <dkwooje@naver.com>
2024-09-11 21:17:37 +0900

    Json 추가

---
+++ 3. JSON 적용/src/main/resources/templates/Postwrite.js
@@ -0,0 +1,26 @@
+[
+    {
+        "title": "과일",
+        "content": "사과"
+    },
+    {
+        "title": "해산물",
+        "content": "꽃게"
+    },
+    {
+        "title": "과일2",
+        "content": "포도"
+    },
+    {
+        "title": "과일3",
+        "content": "복숭아"
+    },
+    {
+        "title": "과일4",
+        "content": "레몬"
+    },
+    {
+        "title": "과일5",
+        "content": "밤"
+    }
+]
